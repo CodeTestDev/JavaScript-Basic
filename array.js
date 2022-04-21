1bde5044005b994aa92f87f78fad41fb5caeea2f
SuperAnil1996 <papa143amit@gmail.com>
2022-04-21 09:22:24 +0530

    new file addd

---
+++ array.js
@@ -0,0 +1,41 @@
+
+console.log("Hello Developers");
+// index array 
+// let myIndexArray=[
+// 	"anil","sunil","rakesh"
+// ];
+
+// console.log(myIndexArray);
+// console.log(myIndexArray[2]);
+
+//object 
+
+// let myObj={
+// 	"name":"Anil",
+// 	"dob":"20/04/2000",
+// 	"father":"",
+// 	"mobile":"9993499863",
+// }
+// console.log(myObj);
+// console.log(myObj.name);
+// console.log(myObj.mobile);
+// console.log(myObj['mobile']);
+//  ObjectName.keyName => value 
+// objectName["mobile"]=> value 
+
+/** mix Arrays and objects **/
+// array of array 
+
+
+	let arrayOfArray=[
+		"anil",
+		[
+			"surname",
+			"Meean"
+		],
+		"Neemuch",
+		"Hello"
+	];
+
+
+	console.log(arrayOfArray)
